9dbfaae89df7b377da93bb4dc1477024f35be478
Артём-ПК <-@DESKTOP-6A2CIVV>
2019-10-14 23:03:42 +0300

    majas sraksri

--- src/lv/jak/artjoms/saraksti/Saraksts.java
+++ src/lv/jak/artjoms/saraksti/Saraksts.java
@@ -19,7 +19,6 @@ public class Saraksts {
 			System.out.println("2-Korīģet audzekņa datus");
 			System.out.println("3-Dzēst audzekni");
 			System.out.println("4-Izdzest visu sarakstu");
-			System.out.println("5-Izvadīt sarakstu  ");
 			System.out.println("0-Beigt barbu ");
 			System.out.print("Izvelaties darbu :");
 			darbiba = input.nextInt();
@@ -45,30 +44,73 @@ public class Saraksts {
 				System.out.println();	
 				break;
 			case 2:
-				int idNr = 2;
-				
-				String jVards;
-				String juzvards;
 				System.out.println("Audzekņa labošana");
-				audzeknisList.get(idNr).setVards(jVards);	
-				audzeknisList.get(idNr).setUzvards(juzvards);
-				audzeknisList.get(idNr).setKurs(2);
-				System.out.println("Izvadiet objekta kārtas numuru");
-				Audzeknis a = audzeknisList.get(idNr - 2);
-				
+				if(audzeknisList.size()==0) {
+					System.out.println("Audzeknu saraksts");
+					System.out.println("----Nav ierakstu----");
+				}
+				else {
+					for(int i=0;i<audzeknisList.size();i++) {
+						System.out.println((i+1)+". Vards:"+audzeknisList.get(i).getVards()
+								+"Uzvards:"+audzeknisList.get(i).getUzvards()
+								+"Kurs:"+audzeknisList.get(i).getKurs());
+					}
+					System.out.println();
+					System.out.println("Ievadit Audzek'na Nr.:");
+					int kartasNr = input.nextInt();
+					if((kartasNr-1)>=audzeknisList.size()||(kartasNr-1)<0){
+						System.out.println("Ievadits nepareizs skaitlis!");
+						
+					}
+					else {
+						System.out.println("Jauna audzekņa pievienošana");
+						System.out.print("Ievadiet  audzekņa vardu:");
+						 vards = input.next();
+						System.out.print("Ievadiet  audzekņa uzvardu:");
+						 uzvards = input.next();
+						System.out.print("Ievadiet  audzekņa kursu:");
+						 kurs = input.nextInt();
+						 audzeknisList.get(kartasNr-1).setVards(vards);
+						 audzeknisList.get(kartasNr-1).setUzvards(uzvards);
+						 audzeknisList.get(kartasNr-1).setKurs(kurs);;
+					}
+				}
 				break;
 			case 3:
 				System.out.println("Audzekņa dzēšana:");
-				audzeknisList.clear();
+				System.out.println("Audzekņa labošana");
+				if(audzeknisList.size()==0) {
+					System.out.println("Audzeknu saraksts");
+					System.out.println("----Nav ierakstu----");
+				}
+				else {
+					for(int i=0;i<audzeknisList.size();i++) {
+						System.out.println((i+1)+". Vards:"+audzeknisList.get(i).getVards()
+								+"Uzvards:"+audzeknisList.get(i).getUzvards()
+								+"Kurs:"+audzeknisList.get(i).getKurs());
+					}
+					System.out.println();
+					System.out.println("Ievadit Audzekna Nr.:");
+					int kartasNr = input.nextInt();
+					audzeknisList.remove(kartasNr);
+					
+					System.out.println("Auzdeknu saraksts:");
+					for (int i = 0; i < audzeknisList.size(); i++) {
+						System.out.println((i+1)+". Vards:"+audzeknisList.get(i).getVards()
+								+"Uzvards:"+audzeknisList.get(i).getUzvards()
+								+"Kurs:"+audzeknisList.get(i).getKurs());
+					}
+						
+					
+				}
+				
 				break;
 			case 4:
-				System.out.println("Saraksta izvadīšana");
-		
-				break;
-			case 5:
-				System.out.println("Saraksta dzēšana");
+				System.out.println("Izdzest visu sarakstu");
 				audzeknisList.removeAll(audzeknisList);
-				
+				//audzeknisList.clear();
+				System.out.println("Saraksta ir dzēsts");
+			
 				break;
 			case 0:
 				System.out.println("Darbs beigts!");
